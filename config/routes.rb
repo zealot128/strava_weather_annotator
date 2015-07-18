8c232cee0da79adc55ab0590ce09b8b14c2aa776
Stefan Wienert <stefan.wienert@pludoni.de>
2015-07-18 12:54:56 +0200

    about

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
   get '/auth/failure' => 'sessions#failure'
+  get '/about' => "pages#about"
 
   resources :trips, only: [:index, :show] do
     collection do
