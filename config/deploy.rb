30f0151a8bfbd11a614a0932646c95cadf12d8a3
Stefan Wienert <stefan.wienert@pludoni.de>
2015-07-18 12:17:12 +0200

    email.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :scm, :git
 # set :log_level, :debug
 set :pty, true
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/email.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 5
