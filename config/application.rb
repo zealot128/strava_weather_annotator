185f8b22ea5eab80db23d1c5e66f48d76f255b1f
Stefan Wienert <info@stefanwienert.de>
2019-04-26 21:30:18 +0200

    Rails app:update 5.0.x

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,16 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module StravaWeather
   class Application < Rails::Application
-
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: false,
