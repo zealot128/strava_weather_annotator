5c3c6c14a100707f4275d91b1bb0c4bd9136e02c
Stefan Wienert <info@stefanwienert.de>
2019-04-28 15:42:19 +0200

    jsonb -> json to support older PG

--- db/migrate/20190426200707_create_trip_streams.rb
+++ db/migrate/20190426200707_create_trip_streams.rb
@@ -2,7 +2,7 @@ class CreateTripStreams < ActiveRecord::Migration[5.2]
   def change
     create_table :trip_streams do |t|
       t.belongs_to :trip, foreign_key: true
-      t.jsonb :data
+      t.json :data
 
       t.timestamps
     end
