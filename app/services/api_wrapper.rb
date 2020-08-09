c61693e680dfd02fb2be2aa74fc06e99d66dfb29
Stefan Wienert <info@stefanwienert.de>
2020-08-09 12:14:31 +0200

    Fix: Auto Refresh Token on Auth Failure

--- app/services/api_wrapper.rb
+++ app/services/api_wrapper.rb
@@ -4,25 +4,28 @@ class ApiWrapper
     @user = user
   end
 
-  def fetch_description(trip)
-  end
-
   class ApiWrapper::StravaApiWrapper < ApiWrapper
     def activity_streams(trip_id, keys)
       log "GET activity"
-      api.activity_streams(trip_id, keys: keys)
+      call_api(:activity_streams, trip_id, keys: keys)
     rescue Faraday::ResourceNotFound
       []
+    rescue Strava::Errors::Fault
+      @user.refresh_token!
+      activity_streams(trip_id, keys)
     end
 
     def get_activities(after: 8.weeks.ago, page: 1)
-      activities = api.athlete_activities(after: after, per_page: 100, page: page)
+      activities = call_api(:athlete_activities, after: after, per_page: 100, page: page)
       log 'GET list_athlete_activities'
       activities
+    rescue Strava::Errors::Fault
+      @user.refresh_token!
+      get_activities(after: after, page: page)
     end
 
     def fetch_description(trip)
-      response = api.activity(trip.strava_id)
+      response = call_api(:activity, trip.strava_id)
       log "GET retrieve_an_activity #{trip.strava_id}"
       trip.description = response['description']
       trip.name = response['name']
@@ -31,7 +34,7 @@ class ApiWrapper
     end
 
     def update_description(trip, text, name)
-      data = api.update_activity(id: trip.strava_id, description: text, name: name)
+      data = call_api(:update_activity, id: trip.strava_id, description: text, name: name)
       log "PUT update_an_activity #{trip.strava_id}"
       trip.description = data['description']
       trip.name = data['name']
@@ -42,6 +45,13 @@ class ApiWrapper
 
     def api
       user.strava_client
+    end
+
+    def call_api(api_method, *args)
+      api.send(api_method, *args)
+    rescue Strava::Errors::Fault
+      @user.refresh_token!
+      api.send(api_method, *args)
     end
 
     def log(msg)
